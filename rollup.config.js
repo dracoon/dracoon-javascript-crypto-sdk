aac7992250baa333f849ada44bfd19ae050ce73f
Florian Loher <f.loher@dracoon.com>
2023-10-18 09:26:34 +0200

    fix(config): adjust config to release pipeline
    closes SDKCRYPTJS-112

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,13 @@
-import commonjs from '@rollup/plugin-commonjs';
-import nodeResolve from '@rollup/plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2';
 
 export default [
     {
         input: 'src/index.ts',
         output: {
-            file: 'lib/bundle.js',
+            file: 'dist/bundle.js',
             format: 'cjs'
         },
         external: ['node-forge'],
         plugins: [typescript()]
-    },
-    {
-        input: 'src/index.ts',
-        output: {
-            file: 'dist/bundle.js',
-            format: 'iife',
-            name: 'Dracoon'
-        },
-        plugins: [commonjs(), nodeResolve({ browser: true }), typescript({ tsconfigOverride: { compilerOptions: { declaration: false } } })]
     }
 ];
